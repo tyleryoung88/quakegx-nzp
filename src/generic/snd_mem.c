437d46baee0f1efd951bf78fdeb12d12a7fb1f45
Tyler Young <tyleryoung88@yahoo.com>
2024-05-25 18:10:15 -0400

    [CLIENT] Add functional bprint and find correct sound folder

--- src/generic/snd_mem.c
+++ src/generic/snd_mem.c
@@ -111,8 +111,8 @@ sfxcache_t *S_LoadSound (sfx_t *s)
 
 //Con_Printf ("S_LoadSound: %x\n", (int)stackbuf);
 // load it in
-    strcpy(namebuffer, "sound/");
-    strcat(namebuffer, s->name);
+   // strcpy(namebuffer, "sound/");
+	Q_strcpy(namebuffer, s->name);
 
 //	Con_Printf ("loading %s\n",namebuffer);
 
